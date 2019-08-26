085b5b419d4bf17f98db94cd81ca6b2f59302cef
karslio <karslio@users.noreply.github.com>
2019-08-26 17:28:38 +0200

    odev2 bitti

--- odev2/odev2.py
+++ odev2/odev2.py
@@ -1,7 +1,6 @@
 # 1.ODEV: BURCUN NE?
 # 	Kullanicidan dogum gununu ve ayini isteyin, program hangi burc oldugunu soylesin.
-# gun = int(input('dogdugunuz gunu girebilir misiniz:'))
-# ay = (input('dogdugunuz ayi girebilir misiniz:'))
+
 # Koç Burcu : 21 Mart - 20 Nisan
 # Boğa Burcu : 21 Nisan - 21 Mayıs
 # İkizler Burcu : 22 Mayıs - 22 Haziran
@@ -15,32 +14,35 @@
 # Kova Burcu : 22 Ocak - 19 Şubat
 # Balık Burcu : 20 Şubat - 20 Mart
 
-# if ay == 'mart' and gun >= 21 or ay == 'nisan' and gun <= 20:
-#     print('Koc Burcu')
-# elif ay == 'nisan' and gun >= 21 or ay == 'mayis' and gun <= 21:
-#     print('Boğa Burcu')
-# elif ay == 'mayis' and gun >= 22 or ay == 'haziran' and gun <= 22:
-#     print('İkizler Burcu')
-# elif ay == 'haziran' and gun >= 23 or ay == 'temmuz' and gun <= 22:
-#     print('Yengeç Burcu')
-# elif ay == 'temmuz' and gun >= 23 or ay == 'agustos' and gun <= 22:
-#     print('Aslan Burcu')
-# elif ay == 'agustos' and gun >= 23 or ay == 'eylul' and gun <= 22:
-#     print('Başak Burcu')
-# elif ay == 'eylul' and gun >= 23 or ay == 'ekim' and gun <= 22:
-#     print('Terazi Burcu')
-# elif ay == 'ekim' and gun >= 23 or ay == 'kasim' and gun <= 21:
-#     print('Akrep Burcu')
-# elif ay == 'kasim' and gun >= 22 or ay == 'aralik' and gun <= 21:
-#     print('Yay Burcu')
-# elif ay == 'aralik' and gun >= 22 or ay == 'ocak' and gun <= 21:
-#     print('Oğlak Burcu')
-# elif ay == 'ocak' and gun >= 22 or ay == 'subat' and gun <= 19:
-#     print('Kova Burcu')
-# elif ay == 'subat' and gun >= 20 or ay == 'mart' and gun <= 21:
-#     print('Balık Burcu')
-# else:
-#     print('girdiginiz degerleri kontrol ediniz')
+gun = int(input('dogdugunuz gunu girebilir misiniz:'))
+ay = (input('dogdugunuz ayi girebilir misiniz:'))
+
+if ay == 'mart' and gun >= 21 or ay == 'nisan' and gun <= 20:
+    print('Koc Burcu')
+elif ay == 'nisan' and gun >= 21 or ay == 'mayis' and gun <= 21:
+    print('Boğa Burcu')
+elif ay == 'mayis' and gun >= 22 or ay == 'haziran' and gun <= 22:
+    print('İkizler Burcu')
+elif ay == 'haziran' and gun >= 23 or ay == 'temmuz' and gun <= 22:
+    print('Yengeç Burcu')
+elif ay == 'temmuz' and gun >= 23 or ay == 'agustos' and gun <= 22:
+    print('Aslan Burcu')
+elif ay == 'agustos' and gun >= 23 or ay == 'eylul' and gun <= 22:
+    print('Başak Burcu')
+elif ay == 'eylul' and gun >= 23 or ay == 'ekim' and gun <= 22:
+    print('Terazi Burcu')
+elif ay == 'ekim' and gun >= 23 or ay == 'kasim' and gun <= 21:
+    print('Akrep Burcu')
+elif ay == 'kasim' and gun >= 22 or ay == 'aralik' and gun <= 21:
+    print('Yay Burcu')
+elif ay == 'aralik' and gun >= 22 or ay == 'ocak' and gun <= 21:
+    print('Oğlak Burcu')
+elif ay == 'ocak' and gun >= 22 or ay == 'subat' and gun <= 19:
+    print('Kova Burcu')
+elif ay == 'subat' and gun >= 20 or ay == 'mart' and gun <= 21:
+    print('Balık Burcu')
+else:
+    print('girdiginiz degerleri kontrol ediniz')
 
 
 # 2.ODEV: UZAKLIK BIRIMI DONUSUMU
@@ -48,23 +50,24 @@
 # 1-km-mil donusumu mu yapmak istiyor, mil-km donusumu mu yapmak istiyor.
 # 2-donusturmek istedigi birimin uzunlugu kactir?
 # Donusum yapilacak birimler mil ve kilometre olacak.
-# deger = float(input('bir deger giriniz:'))
-# print("""
-# *************************
-# islemi seciniz
-# kmTomil = 1
-# milTokm = 2
-# *************************
-# """)
-# kmTomil = '{:04.2f}'.format(deger*0.621371192)
-# milTokm = '{:04.2f}'.format(deger*1.609)
-# islem = int(input('yapmak istediginiz islem ?'))
-# if islem == 1:
-#     print(f'{deger} km {kmTomil} mildir')
-# elif islem == 2:
-#     print(f'{deger} mil {milTokm} kmdir')
-# else:
-#     print('girdiginiz degeri kontrol ediniz')
+
+deger = float(input('bir deger giriniz:'))
+print("""
+*************************
+islemi seciniz
+kmTomil = 1
+milTokm = 2
+*************************
+""")
+kmTomil = '{:04.2f}'.format(deger*0.621371192)
+milTokm = '{:04.2f}'.format(deger*1.609)
+islem = int(input('yapmak istediginiz islem ?'))
+if islem == 1:
+    print(f'{deger} km {kmTomil} mildir')
+elif islem == 2:
+    print(f'{deger} mil {milTokm} kmdir')
+else:
+    print('girdiginiz degeri kontrol ediniz')
 
 # 3.ODEV: PAROLA KARAKTER KONTROLU
 # Kullanicidan 3-18 karakter arasinda bir username olusturmasini isteyin. Eger usernamede rakam varsa,
@@ -74,22 +77,22 @@
 # Iki durumun sartlari da saglaniyorsa username ve parolayi hem ekrana printleyin hem de bir dosyaya kaydedin.
 
 
-# while True:
-#     s = input("username olarak 3-18 karakter arasinda bir deger giriniz: ")
-#     if any(i.isdigit() for i in s):
-#         print("username de sayi kullanamazsin")
-#     elif len(s) <= 3 or len(s) >= 18:
-#         print('username  3 ile 18 karekter olabilir')
-#     else:
-#         print('username basili bir sekilde olusturuldu')
-#         while True:
-#             p = input('parola olarak  6 ile 12 araliginda karekter giriniz')
-#             if len(p) < 6 or len(p) > 18:
-#                 print('lutfen paraloyi istenen sartlarda olusturun')
-#             else:
-#                 print(f'username\t:{s}\nparola\t\t:{p}')
-#                 break
-#     break
+while True:
+    s = input("username olarak 3-18 karakter arasinda bir deger giriniz: ")
+    if any(i.isdigit() for i in s):
+        print("username de sayi kullanamazsin")
+    elif len(s) <= 3 or len(s) >= 18:
+        print('username  3 ile 18 karekter olabilir')
+    else:
+        print('username basili bir sekilde olusturuldu')
+        while True:
+            p = input('parola olarak  6 ile 12 araliginda karekter giriniz')
+            if len(p) < 6 or len(p) > 18:
+                print('lutfen paraloyi istenen sartlarda olusturun')
+            else:
+                print(f'username\t:{s}\nparola\t\t:{p}')
+                break
+    break
 
 
 # 4.ODEV: SAYI TAHMIN OYUNU
@@ -97,30 +100,29 @@
 # Kullanici 5 denemede bilirse 1 yildiz kazansin, 3 denemede bilirse 2 yildiz kazansin, 1 denemede bilirse
 # 3 yildiz kazansin.
 
-# sayi = 7
-# count = 0
-# while count < 6:
-#     tahmin = int(input('1 ile 10 arasiindaki sayiyi tahmin ediniz'))
-#     if tahmin == sayi and count <= 1:
-#         print('sayiyi bildiniz ***')
-#         break
-#     elif tahmin == sayi and count <= 3:
-#         print('sayiyi bildiniz **')
-#         break
-#     elif tahmin == sayi and count <= 5:
-#         print('sayiyi bildiniz *')
-#         break
-#     else:
-#         if tahmin < sayi:
-#             print('daha buyuk bir sayi dene')
-#             count += 1
-#         else:
-#             print('daha kucuk bir sayi dene')
-#             count += 1
-#         if count >= 5:
-#             print('hakkiniz bitmistir')
-#             break
-#
+sayi = 7
+count = 0
+while count < 6:
+    tahmin = int(input('1 ile 10 arasiindaki sayiyi tahmin ediniz'))
+    if tahmin == sayi and count <= 1:
+        print('sayiyi bildiniz ***')
+        break
+    elif tahmin == sayi and count <= 3:
+        print('sayiyi bildiniz **')
+        break
+    elif tahmin == sayi and count <= 5:
+        print('sayiyi bildiniz *')
+        break
+    else:
+        if tahmin < sayi:
+            print('daha buyuk bir sayi dene')
+            count += 1
+        else:
+            print('daha kucuk bir sayi dene')
+            count += 1
+        if count >= 5:
+            print('hakkiniz bitmistir')
+            break
 
 
 # 5.ODEV: ATM
@@ -155,7 +157,7 @@ while True:
         if cekilenMiktar > bakiye:
             print('bakiye yetersiz')
         else:
-            bakiye = bakiye -cekilenMiktar
+            bakiye = bakiye - cekilenMiktar
             print(f'Bakiyeniz {bakiye} Euro kalmistir')
     else:
         print('gecersiz islem')
