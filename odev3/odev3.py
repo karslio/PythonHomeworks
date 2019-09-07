e6406315b8b51b3ae0e21ad16df5e05ea826abda
karslio <karslio@users.noreply.github.com>
2019-09-07 16:25:07 +0200

    odev2

--- odev3/odev3.py
+++ odev3/odev3.py
@@ -7,31 +7,47 @@
 # print(deger.upper())
 
 # 2-)Kullanıcıdan bir input alınız. Girmiş olduğu inputta büyük harf sayısı, küçük harf sayısı,
-# rakam sayısı ve bunların haricindeki
-# özel karakter sayılarını veren bir program yazınız.
+# rakam sayısı ve bunların haricindeki özel karakter sayılarını veren bir program yazınız.
 
-# s = input("bir deger giriniz:")
-# digit = 0
-# if any(i.isdigit() for i in s):
-#     digit +=1
-# print(digit)
-
-
-while True:
-    s = input("Bir sayı girin: ")
-    if s == "iptal":
-        break
-    if len(s) <= 3:
-        continue
-    print("En fazla üç haneli bir sayı girebilirsiniz.")
-
-
+# kelime = input("Herhangi bir kelime: ")
+# buyuk=kucuk=rakam=haric=0
+# for harf in kelime:
+#     if(harf.isupper()):
+#        buyuk+=1
+#     elif(harf.islower()):
+#         kucuk+=1
+#     elif (harf.isdigit()):
+#         rakam+=1
+#     else:
+#         haric+=1
+# print(f'buyuk harf sayisi {buyuk}\nkucuk harf sayisi {kucuk}\nrakam sayisi {rakam}\ndiger karakter {haric}')
 
 # 3-)Kullanıcıdan bir input alınız. Girmiş olduğu inputtaki rakamların toplamını veren bir program yazınız.
 # (Kullanıcı rakam girmek zorunda değil.
 # farklı karakter girişi de yapabilir.)
 # Örnek input = "art12kl4*"
 
+# kelime = input("Herhangi bir kelime: ")
+# toplam=0
+# for sayi in kelime:
+#     if (sayi.isdigit()):
+#         toplam+=int(sayi)
+#
+# print(f'girdiginiz inputtaki sayilar toplami: {toplam}')
+
 # 4-)Futbolcular dosyasındaki futbolculardan ismi sesli harf ile başlayanları ayrı bir dosyaya yazdırınız.
+# f = open('futbolcular', 'r')
+# f2 = open('sesliler.txt', 'w')
+# sesli = 'aeiou'
+# for i in f:
+#     if (i[0].lower() in sesli):
+#         print(i, end='', file=f2)
+
 # 5-)Futbolcular dosyasındaki futbolcu isimlerini yazdığınız program ile Türkçe karakter içermeyecek bir
 # hale getirip yeni bir dosyaya kaydediniz.
+f = open('futbolcular', 'r')
+ftrans = open('translate.txt', 'w')
+kaynak = "şçöğüıŞÇÖĞÜİ"
+hedef = "scoguiSCOGUI"
+for i in f:
+    print(i.translate(str.maketrans(kaynak, hedef)), file=ftrans)
---
+++ odev4/odev4.py
@@ -0,0 +1,68 @@
+# TO DO LIST
+# -Yapilacaklar listesi ve yapilanlar listesi seklinde iki bos liste olusturun. Kullanicinin yapabilecegi islemler:
+#     *Yapilacaklar listesine gorev ekleme
+#         ->append metodu kullanilsin
+#     *Yapilacaklar listesinden gorev silme
+#         -> del metodu kullanilsin
+#     *Yapilacaklar listesindeki gorevi yapilanlar listesine atama
+# #         -> pop metodu kullanilsin
+#     *Yapilanlar listesini goruntuleme
+#     *kullanıcı yapılacaklar listesinin içini boşaltma
+#         -> clear metodu kullanılsın
+# -Dongu her basa dondugunde yapilacaklar listesi goruntulensin
+# -Her iki liste goruntulenirken liste elemanlari numaralandirilarak verilsin
+#     ORN:
+#         Yapilacaklar Listesi:
+#             1- Python odevini yap.
+#             2- Alis-veris yapmaya git.
+#             3- Ahmet'i ara.
+# - Eger yapilacaklar listesi bos ise "Su an yapilacaklar listeniz bos" seklinde bir cikti versin
+
+
+# for i,j in enumerate(yapilacak):
+# print(i+1,'-',j)
+
+yapilacak = []; yapilan = []
+print(len(yapilacak))
+while True:
+    print('gorev ekleme: 1',
+          'gorev silme: 2',
+          'gorevi yapilanlar listesine atama: 3',
+          'Yapilanlar listesini goruntuleme: 4',
+          'yapılacaklar listesinin içini boşaltma: 5',
+          'programdan cikmak istiyorum: q', sep='\n')
+    islem = input('ne yapmak istiyorsunuz')
+    if islem == 'q':
+        print('programdan cikiliyor')
+        break
+    elif islem == '1':
+        if islem == 'q':
+            print('programdan cikiliyor')
+            continue
+        else:
+            todo = input('eklemek istediginiz gorev nedir :')
+            yapilacak.append(todo)
+            print('Yapilacaklar Listesi:')
+            for i,j in enumerate(yapilacak):
+                print(i+1,'-',j)
+
+    elif islem == '2':
+        sil = input('silinecek islem nedir')
+        yapilacak.remove(sil)
+        yapilan.append(sil)
+        print('Yapilacaklar Listesi:')
+        for i, j in enumerate(yapilacak):
+            print(i + 1, '-', j)
+        print('Yapilanlar Listesi:')
+        for i, j in enumerate(yapilan):
+            print(i + 1, '-', j)
+    elif islem == '4':
+        if len(yapilacak) == 0:
+            print('listeniz bos')
+        else:
+            for i, j in enumerate(yapilacak):
+                print(i + 1, '-', j)
+    elif islem == '5':
+        yapilacak.clear()
+    else:
+        print('lutfen gecerli bir islem giriniz')
